771553f6f9cf48a70fc7d60806d0049da09337d9
github username <github email address>
2020-12-19 23:21:50 -0500

    strong params for user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,19 @@
 class UsersController < ApplicationController
 
     def new
+        @user = User.new 
     end 
 
     def create 
+        @user = 
     end 
 
     def show 
+    end 
+
+    private 
+
+    def user_params 
+        params.require(user).permit(:username, :email, :password)
     end 
 end
