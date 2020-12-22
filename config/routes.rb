28d48304cd1f964123e9de204c1a3b793ee3c393
github username <github email address>
2020-12-21 21:09:50 -0500

    login routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,9 @@ Rails.application.routes.draw do
   get '/signup' => 'users#new'
   post '/signup' => 'users#create'
 
+  get '/login' => 'sessions#new'
+  post '/login' => 'sessions#create'
+
   delete '/logout' => 'sessions#destroy'
 
   resources :reviews
