038b712ba4bf51547c6bd56a365a044795969455
github username <github email address>
2020-12-21 19:31:54 -0500

    user controller index, new, show, create, edit, update

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,20 +1,39 @@
 class UsersController < ApplicationController
 
+    def index 
+        @users = User.all 
+    end 
+
+    
     def new
         @user = User.new
+    end 
+
+    def show 
+        @user = User.find(params[:id])
     end 
 
     def create 
         @user = User.new(user_params)
         if @user.save
             session[:user_id] = @user.id 
-            redirect_to @user
+            redirect_to user_path(@user)
         else 
             render :new
         end 
     end 
 
-    def show 
+    def edit 
+        @user = User.find(params[:id])
+    end 
+ 
+    def update 
+        @user.update(user_params)
+        if @user.valid?
+            redirect_to user_path(@user)
+        else
+            render :edit
+        end 
     end 
 
     private 
