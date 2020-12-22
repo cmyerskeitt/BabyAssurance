d2b373a5eb61e1b1e4612bb6a42e1e5c2d025576
github username <github email address>
2020-12-21 21:28:43 -0500

    update show mthod to !@user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,7 +11,7 @@ class UsersController < ApplicationController
 
     def show 
         @user = User.find_by_id(params[:id])
-        redirect_to '/' if @user
+        redirect_to '/' if !@user
     end 
 
     def create 
