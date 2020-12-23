2cce9c53c636931d96001f0e7ded5be956522750
github username <github email address>
2020-12-23 11:09:02 -0500

    logged_in? helper method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,10 @@
 class ApplicationController < ActionController::Base
     def current_user
-        @current_user ||= User.find_by_id(session[:user_id])
+        @current_user ||= User.find_by_id(session[:user_id]) if session[:user_id]
     end 
 
     def logged_in?
+        !!session[:user_id]
     end
 
     def redirect_if_not_logged_in
