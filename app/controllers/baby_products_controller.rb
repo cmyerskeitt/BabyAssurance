0179f9055fa3177e45f57b939a90178bcc7c500e
github username <github email address>
2020-12-23 15:56:24 -0500

    new and index method in babyproductscontroller

--- app/controllers/baby_products_controller.rb
+++ app/controllers/baby_products_controller.rb
@@ -1,3 +1,12 @@
 class BabyProductsController < ApplicationController
     before_action :redirect_if_not_logged_in
+
+    def index
+        @bp = BabyProduct.all
+    end 
+
+    def new 
+        @bp = BabyProduct.new 
+    end 
+
 end
