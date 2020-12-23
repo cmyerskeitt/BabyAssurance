fc347c4381db8a5bab6f7627234038d6126734eb
github username <github email address>
2020-12-23 10:38:37 -0500

    troubleshoot login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,8 +1,21 @@
 class SessionsController < ApplicationController
+    # skip_before_action :verify_authenticity_token
+
     def home
     end 
 
     def new
+    end 
+
+    def create 
+        user = User.find_by(username: params[:username])
+        if user && user.authenticate(params[:password])
+            session[:user_id] = user.id
+            redirect_to user_path(user)
+        else
+            flash[:message] = "Incorrect Credentials, please try again!"
+            redirect_to "/login"
+        end
     end 
 
     def destroy
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,19 +9,20 @@ class UsersController < ApplicationController
         @user = User.new
     end 
 
-    def show 
-        @user = User.find_by_id(params[:id])
-        redirect_to '/' if !@user
-    end 
 
     def create 
         @user = User.new(user_params)
         if @user.save
             session[:user_id] = @user.id 
-            redirect_to user_path(@user)
+            redirect_to @user
         else 
             render :new
         end 
+    end 
+
+    def show 
+        @user = User.find_by_id(params[:id])
+        redirect_to '/' if !@user
     end 
 
     def edit 
@@ -31,7 +32,7 @@ class UsersController < ApplicationController
     def update 
         @user.update(user_params)
         if @user.valid?
-            redirect_to user_path(@user)
+            redirect_to @user
         else
             render :edit
         end 
