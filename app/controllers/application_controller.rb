46e71d2005a05292d9c03562344edc5ba24645bd
github username <github email address>
2020-12-23 10:56:52 -0500

    create current user helper method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,11 @@
 class ApplicationController < ActionController::Base
+    def current_user
+        @current_user ||= User.find_by_id(session[:user_id])
+    end 
+
+    def logged_in?
+    end
+
+    def redirect_if_not_logged_in
+    end 
 end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,12 +8,14 @@ class SessionsController < ApplicationController
     end 
 
     def create 
+        # binding.pry 
         user = User.find_by(username: params[:username])
         if user && user.authenticate(params[:password])
             session[:user_id] = user.id
             redirect_to user_path(user)
         else
-            flash[:message] = "Incorrect Credentials, please try again!"
+            # binding.pry
+            # flash[:message] = "Incorrect Credentials, please try again!"
             redirect_to "/login"
         end
     end 
