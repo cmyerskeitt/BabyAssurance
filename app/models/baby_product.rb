a26dc9483927e970a9cab16f8b9697a309b7cf5d
github username <github email address>
2020-12-27 20:44:59 -0500

    baby_product validations

--- app/models/baby_product.rb
+++ app/models/baby_product.rb
@@ -3,5 +3,5 @@ class BabyProduct < ApplicationRecord
   # belongs_to :brand
   has_many :reviews 
   has_many :users, through: :reviews
-
+  validates :name, :price, :product_type, :image, presence: true 
 end
