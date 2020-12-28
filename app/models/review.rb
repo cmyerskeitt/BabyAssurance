a13ea813ff3043de4d45a12070106ba1ab9df00f
github username <github email address>
2020-12-27 20:39:32 -0500

    validations for username, email, rating, description

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,4 +1,8 @@
 class Review < ApplicationRecord
   belongs_to :user
   belongs_to :baby_product
+
+  validates :rating, numericality: {less_than_or_equal_to: 5, greater_than: 0}
+  validates :description, presence: true
+  
 end
