9b8fea72248bd5fe70fe3649335ab10b2ef97405
github username <github email address>
2021-01-03 21:10:08 -0500

    index view and home display baby product count

--- app/helpers/baby_products_helper.rb
+++ app/helpers/baby_products_helper.rb
@@ -1,2 +1,20 @@
 module BabyProductsHelper
+    def index_display_header
+        if @user
+          content_tag(:h1, "#{@user.username}'s Baby Products:")
+        else
+          content_tag(:h2, "All Baby Products")
+        end
+      end
+    
+      def display_baby_products
+        if @user.baby_products.empty?
+          tag.h2(link_to('No baby products yet - add a baby product here', new_baby_product_path))
+        else
+          user = @user == current_user ? 'You have' : "#{@user.username}'s"
+          content_tag(:h2, "#{user} #{pluralize(@user.baby_products.count, 'Baby Products')}.")
+    
+        end
+      end
+
 end
