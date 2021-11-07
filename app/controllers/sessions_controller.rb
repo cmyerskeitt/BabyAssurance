5fe0276f68f8ea1edd918e26747c51dabf999a01
github username <cmyerskeitt@gmail.com>
2021-11-07 18:42:51 -0500

    change @reviews back to object in layout errors.html.erb and remove errors from edit page

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -12,7 +12,7 @@ class SessionsController < ApplicationController
     end 
 
     def create 
-    
+        
         user = User.find_by(username: params[:username])
         
         if user && user.authenticate(params[:password])
